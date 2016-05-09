0805d1b3fed50d9211a9f5c538aa851a20a1c021
Vaclav Hausenblas <vaclav.hausenblas@gmail.com>
2016-05-09 17:31:42 +0200

    function names convention

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -1,5 +1,13 @@
+#' generate simulated cross-section data for testing purposes
+#' @param N number of observations
+#' @param x_coeff coefficients for explanatory variables
+#' @param z_coeff coefficients for exogeneous varaibles
+#' @param sigma_u variance of the inefficiency term
+#' @param sigma_v variance of the random noise
+#' @param ineff production (-1) or cost inefficiency form (1),
+#' @param aslist TRUE/FALSE to return list or array
 #' @export
-sim_data <- function(N = 500,
+sim_data_cs <- function(N = 500,
                      x_coeff = c(10, 6, 3),
                      z_coeff = c(3, 1.5, 0.5),
                      sigma_u = 2,
@@ -58,44 +66,63 @@ sim_data <- function(N = 500,
     return(cbind(y, X, Z, u, v, eps))
 }
 
+#' generate simulated panel data for testing purposes
+#' @param k number of individuals - cross section
+#' @param t number of observations per individual
+#' @param x_coeff coefficients for explanatory variables
+#' @param z_coeff coefficients for exogeneous varaibles
+#' @param sigma_u variance of the inefficiency term
+#' @param sigma_v variance of the random noise
+#' @param ineff production (-1) or cost inefficiency form (1),
+#' @param aslist TRUE/FALSE to return list or array
 #' @export
-sim_panel_data <- function(obs = 10,
-                           k = 20,
+sim_data_panel <- function(k = 20, # number of individuals - cross section
+                           t = 10, # number of observations per individual
+                           x_mean = 10,
+                           x_sd = 1,
+                           z_mean = 10,
+                           z_sd = 2,
                            x_coeff = c(10, 6, 3),
-                           z_coeff = c(5, 1.9, -0.9),
-                           sigma_u = 2,
-                           sigma_v = 3,
+                           z_intercept = 5,
+                           z_coeff = c(1.9, -0.9),
+                           sigma_u = 2, # variance of the inefficiency term
+                           sigma_v = 3, # variance of the random noise
                            ineff = -1,
                            aslist = F) {
 
-  N <- obs * k
+  N <- t * k
   require(msm)
 
   x_ncols <- length(x_coeff) - 1
-  z_ncols <- length(z_coeff) - 1
+  z_ncols <- length(z_coeff)
 
+  # random explanatory data
   X <- matrix(rtnorm(n = N*x_ncols,
-                     mean = 5,
-                     sd = 3,
+                     mean = x_mean,
+                     sd = z_sd,
                      lower = 0),
               nrow = N,
               ncol = x_ncols)
 
   colnames(X) <- paste0("x", 1:x_ncols)
 
-  mu <- rep(rnorm(k, z_coeff[1], 6), each = k)
+  # random exogeneous data
+
+  # intercept for each individual (cross section)
+  mu_unique <- rnorm(k, z_intercept, 2)
+  mu <- rep(mu_unique, each = t)
 
   if (z_ncols > 0 ) {
     Z <- matrix(rnorm(n = N*z_ncols,
-                      mean = 3,
-                      sd = 2),
+                      mean = z_mean,
+                      sd = z_sd),
                 nrow = N,
                 ncol = z_ncols)
 
     colnames(Z) <- paste0("z", 1:z_ncols)
 
     u <- rtnorm(N,
-                mean = mu + as.vector(Z %*% z_coeff[-1]),
+                mean = mu + as.vector(Z %*% z_coeff),
                 lower = 0,
                 sd = sigma_u)
   } else {
@@ -107,6 +134,7 @@ sim_panel_data <- function(obs = 10,
     Z <- NULL
   }
 
+  # error terms
   v <- rnorm(N,
              mean = 0,
              sd = sigma_v)
@@ -119,7 +147,7 @@ sim_panel_data <- function(obs = 10,
   if (aslist) {
     return(list(y = y, X = X, Z = Z,
                 u = u, v = v, eps = eps,
-                N = N, obs = obs, k = k))
+                N = N, t = t, k = k, mu = mu_unique))
   } else
     return(cbind(y, X, Z, u, v, eps))
 }
