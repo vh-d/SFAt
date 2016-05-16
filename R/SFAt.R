e057a937a28489e2e47815bf487e033ebc4afb81
Vaclav Hausenblas <vaclav.hausenblas@gmail.com>
2016-05-16 11:57:10 +0200

    new package documentation

---
+++ R/SFAt.R
@@ -0,0 +1,5 @@
+#' Package for doing Stochastic frontier analysis (SFA) based on traditional statistical methods on (potentially heterogenous) cross-section and panel data
+#'
+#' This package is a follow-up/was inspired by some earlier R packages for SFA (frontier, sfa, Benchmarking) and Stata frontier, xtfrontier and sfcross/sfpanel routines.
+#'
+"_PACKAGE"
