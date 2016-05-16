643efef263638fb837e16c94ee77bd8de72d81ef
Vaclav Hausenblas <vaclav.hausenblas@gmail.com>
2016-05-16 08:32:34 +0200

    ll functions reparametrized for log(sigma2_u) and log(sigma2_v)

--- R/model_exp.R
+++ R/model_exp.R
@@ -1,10 +1,12 @@
 # NORMAL / EXPONENTIAL - HOMOSCEDASTIC - CROSS SECTION DATA ----------------------------
 
-par_cs_exp <- c(sigma2_u = 1, sigma2_u = 1)
+par_cs_exp <- c(lnsigma2_u = 0, lnsigma2_v = 0)
 
 # likelihood function normal/exponential distributional assumption
-# params: beta, sigma_u^2, sigma_v^2
+# params: beta, log(sigma_u^2), log(sigma_v^2)
+
 ll_cs_exp <- function(params, y, X, ineff, deb) {
+
   nbetas <- ncol(X)
 
   if (length(params) != nbetas + 2) {
@@ -13,19 +15,21 @@ ll_cs_exp <- function(params, y, X, ineff, deb) {
 
   beta_coef <- params[1:nbetas]
 
-  sigma2_u <- params[nbetas + 1]
-  sigma2_v <- params[nbetas + 2]
+  lnsigma2_u <- params[nbetas + 1]
+  lnsigma2_v <- params[nbetas + 2]
+
+  sigma2_u <- exp(lnsigma2_u)
+  sigma2_v <- exp(lnsigma2_v)
+
+  sigma_u <- sqrt(sigma2_u)
+  sigma_v <- sqrt(sigma2_v)
 
   if (deb) cat("Total of ", length(params), " parameters: \n",
                "Betas: ", paste(beta_coef), "\n",
-               "Sigma_u^2: ", sigma2_u,
-               "Sigma_v^2: ", sigma2_v, "\n")
-
-  if (sigma2_u <= 0 | sigma2_v <= 0) return(1e12)
+               "Sigma_u: ", sigma_u,
+               "Sigma_v: ", sigma_v, "\n")
 
   epsilon <- -ineff * (y - X %*% beta_coef)
-  sigma_u <- sqrt(sigma2_u)
-  sigma_v <- sqrt(sigma2_v)
 
   N <- length(y)
 
@@ -38,21 +42,29 @@ ll_cs_exp <- function(params, y, X, ineff, deb) {
   return(-ll)
 }
 
-u_cs_exp <- function(object, type = "JLMS") {
+u_cs_exp <- function(object, estimator) {
 
   # extract sigmas from model object
-  sigma2_u <- object$parameters[length(object$coefficients) + 1]
-  sigma2_v <- object$parameters[length(object$coefficients) + 2]
-  sigma_v <- sqrt(sigma2_v)
+  lnsigma2_u <- object$parameters[length(object$coeff) + 1]
+  lnsigma2_v <- object$parameters[length(object$coeff) + 2]
+
+  # derive the rest of the parameters
+  sigma2_u <- exp(lnsigma2_u)
+  sigma2_v <- exp(lnsigma2_v)
+
   sigma_u <- sqrt(sigma2_u)
+  sigma_v <- sqrt(sigma2_v)
+
+  sigma2 <- sigma2_u + sigma2_v
 
   mu_ast <- object$ineff * object$residuals - sigma2_v / sigma_u
+  sigma_ast <- sqrt(sigma2_u * sigma2_v / sigma2)
 
-  u <- switch(type,
+  u <- switch(estimator,
               ME = pmax(mu_ast, 0),
-              # BC = -log(exp(-mu_ast + 0.5 * sigma_ast^2)*(pnorm(-sigma_ast + mu_ast/sigma_ast)/pnorm(mu_ast/sigma_ast))),
+              BC = -log(exp(-mu_ast + 0.5 * sigma_ast^2)*(pnorm(-sigma_ast + mu_ast/sigma_ast)/pnorm(mu_ast/sigma_ast))),
               JLMS = mu_ast + sigma_v*dnorm(mu_ast/sigma_v)/pnorm(mu_ast/sigma_v))
-  if (is.null(u)) stop(paste0("Unknown type ", type ," of conditional mean estimator."))
+  if (is.null(u)) stop(paste0("Unknown type ", estimator," of conditional mean estimator."))
 
   return(u)
 }
--- R/model_tnorm.R
+++ R/model_tnorm.R
@@ -1,6 +1,9 @@
 # NORMAL / T-NORMAL - HOMOSCEDASTIC - CROSS SECTION DATA ----------------------------
 
-par_cs_tnorm <- c(sigma_u = 1, sigma_v = 1)
+par_cs_tnorm <- c(lnsigma2_u = 0, lnsigma2_v = 0)
+
+# likelihood function normal/truncated-normal distributional assumption
+# params: beta, mu, log(sigma_u^2), log(sigma_v^2)
 
 ll_cs_tnorm <- function(params, y, X, ineff, deb) {
 
@@ -13,8 +16,14 @@ ll_cs_tnorm <- function(params, y, X, ineff, deb) {
   beta_coef <- params[1:nbetas]
 
   mu <- params[nbetas + 1]
-  sigma_u <- params[nbetas + 2]
-  sigma_v <- params[nbetas + 3]
+  lnsigma2_u <- params[nbetas + 2]
+  lnsigma2_v <- params[nbetas + 3]
+
+  sigma2_u <- exp(lnsigma2_u)
+  sigma2_v <- exp(lnsigma2_v)
+
+  sigma_u <- sqrt(sigma2_u)
+  sigma_v <- sqrt(sigma2_v)
 
   if (deb) cat("Total of ", length(params), " parameters: \n",
                "Betas: ", paste(beta_coef), "\n",
@@ -22,11 +31,9 @@ ll_cs_tnorm <- function(params, y, X, ineff, deb) {
                "Sigma_u: ", sigma_u,
                "Sigma_v: ", sigma_v, "\n")
 
-  if (sigma_u <= 0 | sigma_v <= 0) return(1e12)
-
   epsilon <- -ineff * as.vector(y - X %*% beta_coef)
   lambda <- sigma_u / sigma_v
-  sigma <- sqrt(sigma_u^2 + sigma_v^2)
+  sigma <- sqrt(sigma2_u + sigma2_v)
 
   if (deb) cat("lambda: ", lambda,
                "sigma: ", sigma, "\n")
@@ -40,7 +47,8 @@ ll_cs_tnorm <- function(params, y, X, ineff, deb) {
         -0.5*((epsilon + mu) / sigma)^2
     )
 
-  # Kumbhakar 2014 has this function that is actually much more stable
+  # todo: Kumbhakar 2014 has this function that is actually much more stable
+  # check derivation of the likelihood functions
   # ll <-
   #   - N * (log(sigma) + log(pnorm(mu/sigma_u))) +
   #   sum(
@@ -52,27 +60,32 @@ ll_cs_tnorm <- function(params, y, X, ineff, deb) {
 
   return(-ll)
 }
-u_cs_tnorm <- function(object, type) {
+
+# INEFFICIENCY TERM ESTIMATION FUNCTION ------
+u_cs_tnorm <- function(object, estimator) {
   # extract sigmas from model object
-  mu <- object$parameters[length(object$coefficients) + 1]
-  sigma <- object$parameters[length(object$coefficients) + 2]
-  lambda <- object$parameters[length(object$coefficients) + 3]
+  mu <- object$parameters[length(object$coeff) + 1]
+  lnsigma2_u <- object$parameters[length(object$coeff) + 2]
+  lnsigma2_v <- object$parameters[length(object$coeff) + 3]
 
-  sigma_u <- lambda * sigma / sqrt(1 + lambda^2)
-  sigma2_u <- sigma_u^2
+  # derive the rest of the parameters
+  sigma2_u <- exp(lnsigma2_u)
+  sigma2_v <- exp(lnsigma2_v)
 
-  sigma2 <- sigma^2
-  sigma2_v <- sigma2 - sigma2_u
+  sigma_u <- sqrt(sigma2_u)
   sigma_v <- sqrt(sigma2_v)
+
+  sigma2 <- sigma2_u + sigma2_v
 
   mu_ast <- (object$ineff * object$residuals * sigma2_u + sigma2_v * mu) / sigma2
   sigma_ast <- sqrt(sigma2_u * sigma2_v / sigma2)
 
-  u <- switch(type,
+  u <- switch(estimator,
               ME = pmax(mu_ast, 0),
               BC = -log(exp(-mu_ast + 0.5 * sigma_ast^2)*(pnorm(-sigma_ast + mu_ast/sigma_ast)/pnorm(mu_ast/sigma_ast))),
               JLMS = mu_ast + sigma_ast*(dnorm(mu_ast/sigma_ast)/pnorm(mu_ast/sigma_ast)))
-  if (is.null(u)) stop(paste0("Unknown type ", type ," of conditional mean estimator."))
+
+  if (is.null(u)) stop(paste0("Unknown type ", estimator," of conditional mean estimator."))
 
   return(u)
 }
