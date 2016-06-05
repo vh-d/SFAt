fac577e3d7a4a1b4f3ef82d82ab0419d8ac5b660
vh-d <vencahaus@seznam.cz>
2016-06-05 12:43:00 +0200

    heteroskedasticity for half-normal model

--- R/model_hnorm.R
+++ R/model_hnorm.R
@@ -1,6 +1,27 @@
 # NORMAL / H-NORMAL - HOMOSCEDASTIC - CROSS SECTION DATA ----------------------------
 
-par_cs_hnorm <- c(lnsigma2_u = 0.5, lnsigma2_v = 0.5)
+par_cs_hnorm <- NULL
+
+par_cs_hnorm_check <- function(params,
+                               indeces,
+                               y, X,
+                               CM = NULL,
+                               CV_u,
+                               CV_v) {
+
+  # extract parameters from parameter vector
+  n_f_coeff <- ncol(X)
+  n_cv_u_coeff <- if (is.matrix(CV_u)) ncol(CV_u) else length(CV_u)  # number of coeffs for conditional variance of inefficiency term model
+  n_cv_v_coeff <- if (is.matrix(CV_v)) ncol(CV_v) else length(CV_v) # number of coeffs for conditional variance of the symmetric error model
+  # no CM in half-normal model
+
+  if (length(params) != n_f_coeff + n_cv_u_coeff + n_cv_v_coeff) {
+    stop("Incorrect nuber of parameters. ",
+         n_f_coeff, "+", n_cv_u_coeff, "+", n_cv_v_coeff, " needed, but ", length(params), " supplied:", paste(names(params), collapse = " "))
+  } else {
+    return(TRUE)
+  }
+}
 
 t_par_cs_hnorm <- function(pars){
   pars <- sqrt(exp(pars))
@@ -11,42 +32,44 @@ t_par_cs_hnorm <- function(pars){
 # likelihood function normal/half-normal distributional assumption
 # params: beta, log(sigma_u^2), log(sigma_v^2)
 
-ll_cs_hnorm <- function(params, y, X, ineff, deb) {
+ll_cs_hnorm <- function(params,
+                        indeces,
+                        y, X,
+                        CM = NULL,
+                        CV_u,
+                        CV_v,
+                        ineff,
+                        deb) {
 
-  nbetas <- ncol(X)
-
-  if (length(params) != nbetas + 2) {
-    stop("Incorrect nuber of parameters. ", nbetas + 2, " needed, but ", length(params), " supplied.")
+  if (deb) {
+    cat("Parameters: ", params)
   }
 
-  p_beta <- params[1:nbetas]
+  f_coeff    <- params[              1 :indeces[1]]
+  cv_u_coeff <- params[(indeces[1] + 1):indeces[2]]
+  cv_v_coeff <- params[(indeces[2] + 1):indeces[3]]
 
-
-  lnsigma2_u <- params[nbetas + 1]
-  lnsigma2_v <- params[nbetas + 2]
-
-  sigma2_u <- exp(lnsigma2_u)
-  sigma2_v <- exp(lnsigma2_v)
+  sigma2_u <- as.vector(exp(CV_u %*% cv_u_coeff))
+  sigma2_v <- as.vector(exp(CV_v %*% cv_v_coeff))
 
   sigma_u <- sqrt(sigma2_u)
   sigma_v <- sqrt(sigma2_v)
+  sigma <- sqrt(sigma2_u + sigma2_v)
 
   if (deb) cat("Total of ", length(params), " parameters: \n",
-               "Betas: ", paste(p_beta), "\n",
+               "Betas: ", paste(f_coeff), "\n",
+               "Sigma_u: ", sigma_u,
+               "Sigma_v: ", sigma_v, "\n")
 
-               "Sigma_u^2: ", sigma2_u,
-               "Sigma_v^2: ", sigma2_v, "\n")
-
-
-  epsilon <- -ineff*as.vector(y - X %*% p_beta)
-  sigma <- sqrt(sigma2_u + sigma2_v)
+  epsilon <- as.vector(-ineff * (y - X %*% f_coeff))
 
   N <- length(y)
 
   ll <-
     -(N*log(pi/2)) + # the const term
-    -N*log(sigma) +
-    +sum(log(pnorm(-(epsilon * sqrt(sigma2_u) / sqrt(sigma2_v)) / sigma))) - 0.5 / (sigma2_u + sigma2_v) * sum(epsilon^2)
+    + sum(-log(sigma)
+          +log(pnorm(-(epsilon * sqrt(sigma2_u) / sqrt(sigma2_v)) / sigma))
+          -0.5 * (epsilon^2) / (sigma2_u + sigma2_v))
 
   if (deb) {
     if (is.nan(ll)) print(paste(sigma))
@@ -58,15 +81,12 @@ ll_cs_hnorm <- function(params, y, X, ineff, deb) {
 
 # Jondrow et al. (1982) as in Parmeter-Kumbhakar
 u_cs_hnorm <- function(object, estimator) {
-  # extract sigmas from the model object
 
-  lnsigma2_u <- object$parameters[length(object$coeff) + 1]
-  lnsigma2_v <- object$parameters[length(object$coeff) + 2]
+   # extract sigmas from the model object
+  sigma2_u <- as.vector(exp(object$data$CV_u %*% object$coeff_cv_u))
+  sigma2_v <- as.vector(exp(object$data$CV_v %*% object$coeff_cv_v))
 
   # derive the rest of the parameters
-  sigma2_u <- exp(lnsigma2_u)
-  sigma2_v <- exp(lnsigma2_v)
-
   sigma_u <- sqrt(sigma2_u)
   sigma_v <- sqrt(sigma2_v)
 
@@ -78,9 +98,8 @@ u_cs_hnorm <- function(object, estimator) {
   u <- switch(estimator,
               ME = pmax(mu_ast, 0),
               BC = -log(exp(-mu_ast + 0.5 * sigma_ast^2)*(pnorm(-sigma_ast + mu_ast/sigma_ast)/pnorm(mu_ast/sigma_ast))),
-              JLMS = mu_ast + sigma_ast*(dnorm(mu_ast/sigma_ast)/pnorm(mu_ast/sigma_ast)))
-
-  if (is.null(u)) stop(paste0("Unknown type ", estimator," of conditional mean estimator."))
+              JLMS = mu_ast + sigma_ast*(dnorm(mu_ast/sigma_ast)/pnorm(mu_ast/sigma_ast)),
+              ... = stop(paste0("Unknown type ", estimator," of conditional mean estimator.")))
 
   return(u)
 }
