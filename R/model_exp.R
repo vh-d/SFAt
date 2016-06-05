5389b4b550e8e76e91aa5d2b25694f8e4d4e15fe
vh-d <vencahaus@seznam.cz>
2016-06-05 11:59:10 +0200

    heteroskedasticity (conditional variance) in inefficiency and symmetric error terms

--- R/model_exp.R
+++ R/model_exp.R
@@ -1,6 +1,27 @@
 # NORMAL / EXPONENTIAL - HOMOSCEDASTIC - CROSS SECTION DATA ----------------------------
 
-par_cs_exp <- c(lnsigma2_u = 0.5, lnsigma2_v = 0.5)
+par_cs_exp <- NULL
+
+par_cs_exp_check <- function(params,
+                             indeces,
+                             y, X,
+                             CM = NULL,
+                             CV_u,
+                             CV_v) {
+
+  # extract parameters from parameter vector
+  n_f_coeff <- ncol(X)
+  n_cv_u_coeff <- if (is.matrix(CV_u)) ncol(CV_u) else length(CV_u)  # number of coeffs for conditional variance of inefficiency term model
+  n_cv_v_coeff <- if (is.matrix(CV_v)) ncol(CV_v) else length(CV_v) # number of coeffs for conditional variance of the symmetric error model
+  # no CM in exponential model
+
+  if (length(params) != n_f_coeff + n_cv_u_coeff + n_cv_v_coeff) {
+    stop("Incorrect nuber of parameters. ",
+         n_f_coeff, "+", n_cv_u_coeff, "+", n_cv_v_coeff, " needed, but ", length(params), " supplied:", paste(names(params), collapse = " "))
+  } else {
+    return(TRUE)
+  }
+}
 
 t_par_cs_exp <- function(pars){
   pars <- sqrt(exp(pars))
@@ -8,55 +29,58 @@ t_par_cs_exp <- function(pars){
   return(pars)
 }
 
+
+
 # likelihood function normal/exponential distributional assumption
 # params: beta, log(sigma_u^2), log(sigma_v^2)
-ll_cs_exp <- function(params, y, X, ineff, deb) {
+ll_cs_exp <- function(params,
+                      indeces,
+                      y, X,
+                      CM = NULL,
+                      CV_u,
+                      CV_v,
+                      ineff,
+                      deb) {
 
-  nbetas <- ncol(X)
-
-  if (length(params) != nbetas + 2) {
-    stop("Incorrect nuber of parameters. ", nbetas + 2, " needed, but ", length(params), " supplied.")
+  if (deb) {
+    cat("Parameters: ", params)
   }
 
-  beta_coef <- params[1:nbetas]
+  f_coeff    <- params[              1 :indeces[1]]
+  cv_u_coeff <- params[(indeces[1] + 1):indeces[2]]
+  cv_v_coeff <- params[(indeces[2] + 1):indeces[3]]
 
-  lnsigma2_u <- params[nbetas + 1]
-  lnsigma2_v <- params[nbetas + 2]
-
-  sigma2_u <- exp(lnsigma2_u)
-  sigma2_v <- exp(lnsigma2_v)
+  sigma2_u <- as.vector(exp(CV_u %*% cv_u_coeff))
+  sigma2_v <- as.vector(exp(CV_v %*% cv_v_coeff))
 
   sigma_u <- sqrt(sigma2_u)
   sigma_v <- sqrt(sigma2_v)
 
   if (deb) cat("Total of ", length(params), " parameters: \n",
-               "Betas: ", paste(beta_coef), "\n",
+               "Betas: ", paste(f_coeff), "\n",
                "Sigma_u: ", sigma_u,
                "Sigma_v: ", sigma_v, "\n")
 
-  epsilon <- -ineff * (y - X %*% beta_coef)
+  epsilon <- as.vector(-ineff * (y - X %*% f_coeff))
 
   N <- length(y)
 
   ll <-
     - N * log(sigma_u) +
-    0.5 * N * (sigma2_v / sigma2_u) +
-    sum(log(pnorm(-(epsilon + (sigma2_v / sigma_u)) / sigma_v))) +
-    sum(epsilon) / sigma_u
+    sum(0.5 * sigma2_v / sigma2_u +
+          log(pnorm(-(epsilon + (sigma2_v / sigma_u)) / sigma_v)) +
+          epsilon / sigma_u)
 
   return(-ll)
 }
 
 u_cs_exp <- function(object, estimator) {
 
-  # extract sigmas from model object
-  lnsigma2_u <- object$parameters[length(object$coeff) + 1]
-  lnsigma2_v <- object$parameters[length(object$coeff) + 2]
+  # extract sigmas from the model object
+  sigma2_u <- as.vector(exp(object$data$CV_u %*% object$cv_u_coeff))
+  sigma2_v <- as.vector(exp(object$data$CV_v %*% object$cv_v_coeff))
 
   # derive the rest of the parameters
-  sigma2_u <- exp(lnsigma2_u)
-  sigma2_v <- exp(lnsigma2_v)
-
   sigma_u <- sqrt(sigma2_u)
   sigma_v <- sqrt(sigma2_v)
 
@@ -68,8 +92,8 @@ u_cs_exp <- function(object, estimator) {
   u <- switch(estimator,
               ME = pmax(mu_ast, 0),
               BC = -log(exp(-mu_ast + 0.5 * sigma_ast^2)*(pnorm(-sigma_ast + mu_ast/sigma_ast)/pnorm(mu_ast/sigma_ast))),
-              JLMS = mu_ast + sigma_v*dnorm(mu_ast/sigma_v)/pnorm(mu_ast/sigma_v))
-  if (is.null(u)) stop(paste0("Unknown type ", estimator," of conditional mean estimator."))
+              JLMS = mu_ast + sigma_v*dnorm(mu_ast/sigma_v)/pnorm(mu_ast/sigma_v),
+              ... = stop(paste0("Unknown type ", estimator," of conditional mean estimator.")))
 
   return(u)
 }
