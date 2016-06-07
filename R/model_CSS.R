83853e8d6006ed2d2a8045c79565327c364c7c46
vh-d <vencahaus@seznam.cz>
2016-06-07 23:18:54 +0200

    fit inefficiency term

--- R/model_CSS.R
+++ R/model_CSS.R
@@ -12,28 +12,69 @@ fit_css_model <- function(y,
                           deb = T) {
   require(plm)
 
-  formula_frontier <- as.formula(paste0(c("y ~ ", paste0(colnames(pdata$X), collapse = " + ")), collapse = ""))
-  dataframe <- pdata.frame(data.frame(K, y, X), index = c("k", "t"))
-  head(dataframe)
+  formula_frontier <- as.formula(
+    paste0(c("y ~ ",
+             paste0(colnames(pdata$X), collapse = " + "),
+             " - 1"),
+           collapse = ""))
 
-  plmfit <- plm(formula_frontier, data = dataframe, model = "within")
+  dataframe <- pdata.frame(data.frame(K, y, X), index = c("k", "t"))
+
+  if (deb) print(head(dataframe))
+
+  step1fit <- plm(formula_frontier, data = dataframe, model = "within")
+  alpha_0 <- fixef(step1fit)
 
   if (deb) {
-    print(summary(plmfit))
-    print(fixef(plmfit))
+    print(summary(step1fit))
+    print(alpha_0)
   }
 
-  rfit <- plmfit$residuals
+  step2fit <- lm(formula = res ~ as.factor(k):t + as.factor(k):I(t^2),
+                 data    = data.frame(res = step1fit$residuals,
+                                         pdata$K))
 
-  lmfit <- lm(rfit ~ as.factor(k) + as.factor(k):t  + as.factor(k):I(t^2) - 1, data = as.data.frame(pdata$K))
+  if (deb) {
+    print(summary(step2fit))
+  }
+
+  result <- list(
+    fe = alpha_0,
+    tvar = step2fit$coefficients,
+    step1fit = step1fit,
+    step2fit = step2fit
+  )
 
   # lmfit <- summary(lmfit)
 
-  return(lmfit)
+  return(result)
 }
 
-# pdata <- sim_data_panel(aslist = T)
-#
+uit_css <- function(x) {
+  predict(x$step2fit) + rep(x$fe, 10)
+}
+
+# require(SFAt)
+# require(data.table)
+# require(ggplot2)
+# pdata <- sim_data_panel(z_mean = 10:30, sigma_u = 1, sigma_v =1, z_coeff = 0, aslist = T)
 # temp <- fit_css_model(pdata$y, X = pdata$X, K = pdata$K, ineff = -1)
+# plot(temp$fe)
 #
-# print(summary(temp))
+# uit <- data.table(pdata$K, uit = uit_css(temp))
+# uit[, ui := max(uit), by = t]
+# uit[, ineff := ui - uit]
+# # ui <- aggregate(x = uit$uit, by = list(uit$t), FUN = max)
+# # ineff <- rep(ui$x, times = 20) - uit$uit
+# # hist(ineff)
+#
+# ggplot(uit,
+#       aes(x = t,
+#           y = ineff,
+#           color = factor(k))) +
+#   geom_line()
+#
+# ggplot(uit,
+#        aes(x = ineff)) +
+#   geom_histogram()
+#
