a43d0297175a34515dc477ab1f0bc3081f143610
Vaclav Hausenblas <vaclav.hausenblas@gmail.com>
2016-06-07 18:39:57 +0200

    prepare general api for panel data models

---
+++ R/panel.R
@@ -0,0 +1,25 @@
+panel.sfa.fit <- function(y,
+                          X,
+                          # CM = NULL,
+                          # CV_u = NULL,
+                          # CV_v = NULL,
+                          K = NULL,
+                          ineff = -1L,
+                          spec = c("CSS"),
+                          # intercept = list(f = TRUE,
+                          #                  cm = TRUE,
+                          #                  cv_u = TRUE,
+                          #                  cv_v = TRUE),
+                          # sv = list(f = NULL,
+                          #           cm = NULL,
+                          #           cv_u = NULL,
+                          #           cv_v = NULL),
+                          # ll = NULL,
+                          # opt_method = "SANN",
+                          # opt_control = NULL,
+                          deb = F, # TRUE for debug reports
+                          debll = F
+) {
+
+}
+
